bbcc6253bcdf88bfc678cc0368b9c43c5c76a68a
f1xha <an.esquerre@duocuc.cl>
2025-11-21 16:24:54 -0300

    Agrega RemoteRepository para persistencia externa Actualiza AppUiState con lista remota de juegos

--- app/src/main/java/com/example/gamehub/model/AppUiState.kt
+++ app/src/main/java/com/example/gamehub/model/AppUiState.kt
@@ -3,5 +3,8 @@ package com.example.gamehub.model
 data class AppUiState(
     val userEmail: String? = null,
     val loggedIn: Boolean = false,
-    val games: List<GameItem> = emptyList()
+    val games: List<GameItem> = emptyList(),
+
+    // NUEVO: lista de juegos online desde API
+    val remoteGames: List<RemoteGame> = emptyList()
 )
--- app/src/main/java/com/example/gamehub/repository/RemoteRepository.kt
+++ app/src/main/java/com/example/gamehub/repository/RemoteRepository.kt
@@ -1,4 +1,7 @@
 package com.example.gamehub.repository
 
 class RemoteRepository {
+
+    suspend fun fetchRemoteGames() =
+        RetrofitClien.api.getGames()
 }
