fcd7633497428b1ba5099be2bbfcb52ce96ffb7c
f1xha <an.esquerre@duocuc.cl>
2025-11-21 19:25:58 -0300

    Se agregan 2 pruebas para ver si funcionan correctamente

--- app/src/androidTest/java/com/example/gamehub/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/gamehub/ExampleInstrumentedTest.kt
@@ -1,24 +1,59 @@
 package com.example.gamehub
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.example.gamehub.viewmodel.AppViewModel
+import com.example.gamehub.repository.GameRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+class AppViewModelTest {
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+    /**
+     * Test 1:
+     * Si el email NO tiene formato válido, el ViewModel debe devolver "Email inválido"
+     */
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.gamehub", appContext.packageName)
+    fun emailInvalido_retornaError() = runTest {
+        val repo = GameRepository.getInstance()
+        val vm = AppViewModel(repo)
+
+        var error: String? = null
+
+        vm.login(
+            email = "correo_malo",
+            password = "1234",
+            onSuccess = {},
+            onError = { msg -> error = msg }
+        )
+
+        assertEquals("Email inválido", error)
+    }
+
+    /**
+     * Test 2:
+     * Si la contraseña tiene menos de 4 caracteres,
+     * el ViewModel debe devolver el error correspondiente
+     */
+    @Test
+    fun passwordCorta_retornaError() = runTest {
+        val repo = GameRepository.getInstance()
+        val vm = AppViewModel(repo)
+
+        var error: String? = null
+
+        vm.login(
+            email = "test@gmail.com",
+            password = "12",
+            onSuccess = {},
+            onError = { msg -> error = msg }
+        )
+
+        assertEquals("La contraseña debe tener al menos 4 caracteres", error)
     }
 }
+
+    /**
+     * Test 3:
+     * Si email y password son válidos, no debe devolver error
+     * y debe ejecutar onSuccess
+     */
